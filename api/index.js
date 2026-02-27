7a7aac9d7380a17511f01d7bdfee38740661bc44
Salif <salif@loveforyou.com>
2026-02-27 18:03:56 +0530

    Fix Mongoose 9 pre-save hook - remove next callback

--- api/index.js
+++ api/index.js
@@ -36,10 +36,9 @@ const userSchema = new mongoose.Schema({
     avatar: { type: String, default: '💕' },
 }, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 12);
-    next();
 });
 
 userSchema.methods.comparePassword = async function (candidate) {
